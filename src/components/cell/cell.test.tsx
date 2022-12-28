1d98be55997cbf7ff6694f48f809ceaf44b3c21c
Radamir <radamir.gorynin@raiffeisen.ru>
2022-12-28 18:55:17 +0300

    delete handler mock

--- src/components/cell/cell.test.tsx
+++ src/components/cell/cell.test.tsx
@@ -14,9 +14,7 @@ describe('Test handler', () => {
   it('Testing cell to show alive status', () => {
     render(<Cell isAlive={true} />);
     const div = screen.getByTestId(CELL_TEST.testId);
-    const handleClick = jest.fn((x) => x + 1);
     userEvent.click(div);
-    //expect(handleClick).toHaveBeenCalledTimes(1);  почему тут падаем?
     expect(screen.findByText('0')).not.toBeInTheDocument;
     expect(screen.getByText('1')).toBeInTheDocument;
   });
